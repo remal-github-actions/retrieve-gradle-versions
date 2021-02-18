c5945cab97b339ab3dac3c641cf506925cd01729
remal <remal@users.noreply.github.com>
2021-02-18 15:57:23 +0000

    [push-back] Push-back updated files during build

--- dist/index.js
+++ dist/index.js
@@ -416,7 +416,7 @@ run();
 function lastVersionByNumber(versions, pos) {
     const grouped = {};
     for (const version of versions) {
-        const key = version.withNumber(pos, 0).withoutSuffix().toString();
+        const key = version.withoutSuffix().withoutNumber(pos + 1).withNumber(pos, 0).toString();
         const prev = grouped[key];
         if (!prev || prev.compareTo(version) < 0) {
             grouped[key] = version;
